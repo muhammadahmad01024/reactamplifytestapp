6128350e49c0c1cddd92a98ee18121cbd659e618
Numan Abaid <bcsf17m502@pucit.edu.pk>
2020-12-11 11:31:26 +0500

    Second commit with changed messge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello AWS Amplify
         </p>
         <a
           className="App-link"
